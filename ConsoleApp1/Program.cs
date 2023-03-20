0e2806f575217fb918b8ff5b56d39e13b53f81c5
`Vlad` <ap5291433@gmail.com>
2023-03-20 09:05:27 +0200

    версия1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 10;
+            int[] array = { 3, 9, 15, 25, 7, 33, 17, 85, 12, 49 };
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                count += array[i];
+                Console.WriteLine($"{i + 1} Груз={array[i]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Общая масса груза={count}");
+            Console.Read();
         }
     }
 }
